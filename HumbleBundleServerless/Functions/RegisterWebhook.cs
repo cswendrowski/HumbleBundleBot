9d99933803dfd8c1024655d13a080156c4ce5078
Cody Swendrowski <cody.swendrowski@7summitsinc.com>
2017-10-26 19:41:44 -0500

    Adds a Function for registering new Webhooks

---
+++ HumbleBundleServerless/Functions/RegisterWebhook.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using HumbleBundleServerless.Models;
+using System.Linq;
+
+namespace HumbleBundleServerless.Functions
+{
+    public static class RegisterWebhook
+    {
+        [FunctionName("RegisterWebhook")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequestMessage req,
+            [Table("webhookRegistration")] IQueryable<WebhookRegistrationEntity> inTable,
+            [Table("webhookRegistration")] ICollector<WebhookRegistrationEntity> outTable,
+            TraceWriter log)
+        {
+            dynamic data = await req.Content.ReadAsAsync<object>();
+            int type = data?.type;
+            string webhook = data?.webhook;
+            bool recieveUpdates = data?.sendUpdates;
+
+            var lastBundleType = BundleTypes.MONTHLY;
+
+            if (type < 0 || type > (int) lastBundleType)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest, "Please pass in a valid Bundle Type int (0 - " + (int)lastBundleType + ")");
+            }
+
+            var bundleType = (BundleTypes)type;
+
+            if (webhook == null)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a webhook in the request body");
+            }
+
+            if (!webhook.Contains("discordapp.com/api/webhooks/"))
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest, "Please pass in a valid Discord Webhook URL in the request body");
+            }
+
+            if (inTable.ToList().Any(x => x.Webhook == webhook && x.BundleType == type))
+            {
+                return req.CreateResponse(HttpStatusCode.Conflict, "This webhook URL is already registered for this Bundle type");
+            }
+
+            outTable.Add(new WebhookRegistrationEntity(bundleType, webhook, recieveUpdates));
+
+            return req.CreateResponse(HttpStatusCode.Created);
+        }
+    }
+}
---
+++ HumbleBundleServerless/Models/WebhookRegistrationEntity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace HumbleBundleServerless.Models
+{
+    public class WebhookRegistrationEntity : TableEntity
+    {
+        public WebhookRegistrationEntity(BundleTypes type, string webhook, bool shouldRecieveUpdates)
+        {
+            PartitionKey = type.ToString();
+            RowKey = System.Guid.NewGuid().ToString();
+
+            Webhook = webhook;
+            ShouldRecieveUpdates = shouldRecieveUpdates;
+            BundleType = (int) type;
+        }
+
+        public WebhookRegistrationEntity() { }
+
+        public string Webhook { get; set; }
+
+        public bool ShouldRecieveUpdates { get; set; } = false;
+
+        public int BundleType { get; set;  }
+    }
+}
