a8769b2ae35e54bef5a7c1bfc26ed37f8bd8cca3
Cody Swendrowski <cody@swendrowski.us>
2018-12-19 20:29:00 -0600

    Adds missing file

---
+++ HumbleBundleServerless/Models/WebhookResponse.cs
@@ -0,0 +1,17 @@
+﻿using HumbleBundleServerless.Models;
+
+namespace HumbleBundleServerless.Models
+{
+    public class WebhookResponse
+    {
+        public WebhookResponse(WebhookRegistrationEntity created)
+        {
+            Id = created.RowKey;
+            Type = created.PartitionKey;
+        }
+
+        public string Id { get; set; }
+
+        public string Type { get; set; }
+    }
+}
