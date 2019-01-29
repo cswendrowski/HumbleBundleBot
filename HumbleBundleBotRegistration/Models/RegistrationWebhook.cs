6c3d2f1273f1c2baaea7fa327f0ecd3427957a62
Brian <ballbrian.a@gmail.com>
2019-01-29 16:26:57 -0600

    Added Registration and Delete Site

---
+++ HumbleBundleBotRegistration/Models/RegistrationWebhook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HumbleBundleBotRegistration.Models
+{
+    public class RegistrationWebhook : WebhookInfoBase
+    {
+
+        public bool SendUpdates { get; set; }
+
+        public string Partner { get; set; }
+
+    }
+}
---
+++ HumbleBundleBotRegistration/Services/WebhookService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using HumbleBundleBotRegistration.Models;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace HumbleBundleBotRegistration.Services
+{
+    public class WebhookService : IWebhookService
+    {
+
+        private HttpClient _client;
+
+        public WebhookService()
+        {
+            if (_client == null)
+            {
+                _client = new HttpClient();
+            }            
+        }
+
+        public async Task<bool> RegisterWebhook(RegistrationWebhook registrationWebhook)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post,
+                "https://humblebundlenotifications.azurewebsites.net/api/RegisterWebhook")
+            {
+                Content = PrepareWebhookInfo(registrationWebhook)
+            };
+
+            var response = await _client.SendAsync(request);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeregisterWebhook(DeregistrationWebhook deregistrationWebhook)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete,
+                "https://humblebundlenotifications.azurewebsites.net/api/DeleteWebhook")
+            {
+                Content = PrepareWebhookInfo(deregistrationWebhook)
+            };
+
+            var response = await _client.SendAsync(request);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        private StringContent PrepareWebhookInfo<T>(T registrationWebhook)
+        {
+            var serializerSettings = new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore, 
+            };
+            
+            return new StringContent(JsonConvert.SerializeObject(registrationWebhook, serializerSettings), Encoding.UTF8, "application/json");
+        }
+
+    }
+}
